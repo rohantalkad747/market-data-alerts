8d586eb86e476488047cc79f03988c6ee97de672
rohantalkad747 <30855683+rohantalkad747@users.noreply.github.com>
2020-05-16 16:47:16 -0400

    ..

--- src/main/java/com/h2o_execution/alerts/IoI.java
+++ src/main/java/com/h2o_execution/alerts/IoI.java
@@ -1,10 +1,18 @@
 package com.h2o_execution.alerts;
 
-import com.h2o_execution.domain.EnhancedQuote;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Indication of interest is configuration for a market data alert.
+ * There are two types of IoIs: permanent and ephemeral. The former is specified by
+ * the {@linkplain com.h2o_execution.alerts.Threshold.Type#PERCENT} and is updated daily
+ * based on the specified {@linkplain com.h2o_execution.alerts.Threshold.Target target}. However,
+ * if the threshold type is set to {@linkplain com.h2o_execution.alerts.Threshold.Type#ABSOLUTE},
+ * the IoI is deactivated upon reaching the crossing the specified value. An IoI can be currently sent
+ * through three media: TCP, SMS, or SMTP. An IoI can be cancelled at any time given the id and symbol.
+ */
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
